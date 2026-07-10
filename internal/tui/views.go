ede79439f946472f0940322e2f337bf79c4f4f0c
Alex H. Raber <alexhraber@gmail.com>
2026-07-09 19:19:45 -0700

    Bump step name cap to 17+3=20 chars

--- internal/tui/views.go
+++ internal/tui/views.go
@@ -421,8 +421,8 @@ func (m Model) rightPaneView(w, h int) string {
 					_, end := gr.Positions()
 					cw := lipgloss.Width(cluster)
 					name := strings.TrimLeft(label[end:], " ")
-					if len([]rune(name)) > 15 {
-						name = string([]rune(name)[:15]) + "..."
+					if len([]rune(name)) > 17 {
+						name = string([]rune(name)[:17]) + "..."
 					}
 					pad := ""
 					if cw < 2 {
